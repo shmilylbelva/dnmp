5a0d9ce6c51914a77bb13032027a0ccb6ef3808b
shmilylbelva <1028604181@qq.com>
2018-09-22 01:33:57 +0800

    1

--- www/tp5/config/database.php
+++ www/tp5/config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => 'mysql',
     // 数据库名
     'database'        => 'tp5',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => '123456',
     // 端口
     'hostport'        => '',
     // 连接dsn
